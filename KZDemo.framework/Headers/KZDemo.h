dc501bd4042fc3f4ddffbf25ad3c4fc98a558209
Eric <developer_eric@163.com>
2019-02-17 11:43:59 +0800

    [Update] 3.0.0.1

---
+++ KZDemo.framework/Headers/KZDemo.h
@@ -0,0 +1,19 @@
+//
+//  KZDemo.h
+//  KZDemo
+//
+//  Created by zhangshupeng.
+//  Copyright © 2018 kz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KZDemo : NSObject
+
+- (void)test;
+
++ (nonnull NSString *)name;
+
++ (nonnull NSString *)version;
+
+@end
