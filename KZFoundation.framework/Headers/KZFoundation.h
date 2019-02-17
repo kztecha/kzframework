a2da57f68a8cabb8b288e3c8b32e0e65f9277fde
xulong <1689988080@qq.com>
2019-02-17 11:45:52 +0800

    [Update] 2.0.0.1

---
+++ KZFoundation.framework/Headers/KZFoundation.h
@@ -0,0 +1,18 @@
+//
+//  KZFoundation.h
+//  KZFoundation
+//
+//  Created by zhangshupeng.
+//  Copyright © 2018 kz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KZFoundation : NSObject
+
++ (nonnull NSString *)name;
+
++ (nonnull NSString *)version;
+
++ (void)test;
+@end
